176c69d17350fb252eae7ddc22aefbc0b1b68d28
Ihar Baravy <igor.borovoi@datarobot.com>
2025-03-31 23:30:36 +0200

    [TEMPLATE-227] Fix There are multiple elements with the same key='/api/v2/account/apiKeys/' (#160)
    * [TEMPLATE-227] Fix There are multiple elements with the same key='/api/v2/account/apiKeys/'
    
    * sleep

--- frontend/datarobot_connect.py
+++ frontend/datarobot_connect.py
@@ -76,7 +76,7 @@ class DataRobotTokenManager:
     def _get_contents_from_url(self, url: str) -> dict[str, Any]:
         """Fetch data from DataRobot API using JavaScript."""
         js_command = self._JS_COMMAND_TEMPLATE.replace("URL", url)
-        result = st_javascript(js_command, key=url)
+        result = st_javascript(js_command)
         time.sleep(1)
         data = {}
         try:
